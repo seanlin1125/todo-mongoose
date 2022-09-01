ddfa8170e6676094239ab1019ef7e3aae82744ad
seanlin1125 <sean821125@gmail.com>
2022-09-01 15:13:51 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -44,6 +44,14 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
