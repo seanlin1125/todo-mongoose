ac4bad2e01326dacd730f04df9e8665a51c239ea
seanlin1125 <sean821125@gmail.com>
2022-10-07 10:40:34 +0800

    feat: add relations between User and Todo models

--- models/todo.js
+++ models/todo.js
@@ -8,6 +8,12 @@ const todoSchema = new Schema({
   isDone: {
     type: Boolean,
     default: false  // 預設完成狀態為 false
+  },
+  userId: {  // 加入關聯設定
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
   }
 })
 module.exports = mongoose.model('Todo', todoSchema)
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -10,44 +10,49 @@ router.get('/new', (req, res) => {
 })
 // create data
 router.post('/', (req, res) => {
+  const userId = req.user._id
   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
+  return Todo.create({ name, userId })     // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
 // detail page
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Todo.findOne({ _id, userId })
     .lean()
     .then((todo) => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
 // edit page
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Todo.findOne({ _id, userId })
     .lean()
     .then((todo) => res.render('edit', { todo }))
     .catch(error => console.log(error))
 })
 // edit data
 router.put('/:id', (req, res) => {
-  const id = req.params.id
+  const userId = req.user._id
+  const _id = req.params.id
   const { name, isDone } = req.body
-  return Todo.findById(id)
+  return Todo.findOne({ _id, userId })
     .then(todo => {
       todo.name = name
       todo.isDone = isDone === 'on'
       return todo.save()
     })
-    .then(() => res.redirect(`/todos/${id}`))
+    .then(() => res.redirect(`/todos/${_id}`))
     .catch(error => console.log(error))
 })
 // delete data
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Todo.findOne({ _id, userId })
     .then(todo => todo.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
