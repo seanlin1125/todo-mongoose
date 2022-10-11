2a799428a80160b563afb0dc07211f1f81f396ee
seanlin1125 <sean821125@gmail.com>
2022-10-11 11:25:20 +0800

    modify todoSeeder

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
-  res.locals.warning_msg = req.flash('warning_msg') 
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 // 將 request 導入路由器
